21c4031335c053fe834a6f9d2962aa0b1de21393
nikki9119 <a.p.nikhilkumar@gmail.com>
2020-07-05 22:03:38 +0530

    added user and company classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hi there!");
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
